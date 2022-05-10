d99d2b59327a7e0ba1c23b2110579b237c4a56cd
Graeme <graeme_c_smith@hotmail.com>
2022-05-10 21:58:56 +0100

    Initial testing script template

---
+++ src/testing/test_snp_filtering.py
@@ -0,0 +1,21 @@
+import snp_haplotype
+import pytest
+import json
+
+# Import launch.json and parse the file for the correct parameters for each of the samples
+
+# Import the validation data set from informative_snp_validation.json
+
+
+@pytest.fixture()
+def run_dx_jobs():
+
+    return
+
+
+def calculate_informative_snps():
+    pass
+
+
+def calculate_ebryo_snps():
+    pass
