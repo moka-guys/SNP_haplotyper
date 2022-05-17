3f0f431f191cd1021c1171d2d1b28aa21aad6ef0
Graeme <graeme_c_smith@hotmail.com>
2022-05-17 16:53:14 +0100

    Added initial autosomal recessive logic

--- src/snp_haplotyper/autosomal_recessive_logic.py
+++ src/snp_haplotyper/autosomal_recessive_logic.py
@@ -4,97 +4,140 @@ import pandas as pd
 
 def autosomal_recessive_analysis(
     df,
-    affected_partner,
-    unaffected_partner,
+    male_partner,
+    female_partner,
     reference,
     reference_status,
     reference_relationship,
+    consanguineous,
 ):
     """
     TODO: Add docstring
     """
-    if reference_relationship in [
-        "grandparent",  # TODO populate with all appropriate relationships
-    ]:
+    if consanguineous & reference_status == "affected":
         # Label alleles as high or low risk
         conditions = [
-            # Criteria to label high Risk SNPs if reference affected, or low risk SNPs if reference unaffected
+            # Criteria to label low Risk SNPs if reference affected, or low risk SNPs if reference unaffected
+            # Criteria 1
+            (df[reference] == "AA" | df[reference] == "BB")
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "AA" | df[female_partner] == "BB"),
+            # Criteria 2
+            (df[reference] == "AA" | df[reference] == "BB")
+            & (df[male_partner] == "AA" | df[male_partner] == "BB")
+            & (df[female_partner] == "AB"),
+            # Criteria 3
+            (df[reference] == "AA" | df[reference] == "BB")
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "AB"),
+        ]
+        # Assign the correct labels depending upon reference status
+        if consanguineous & reference_status == "affected":
+            values = [
+                "male_partner_low_risk",  # Criteria 1
+                "female_partner_low_risk",  # Criteria 2
+                "shared_high_risk",  # Criteria 3
+            ]
+
+        df["snp_risk_category"] = np.select(conditions, values, default="uninformative")
+
+    elif reference_status == "affected":
+        # Label alleles as high or low risk
+        conditions = [
+            # Criteria to label low Risk SNPs if reference affected, or low risk SNPs if reference unaffected
             # Criteria 1
             (df[reference] == "AA")
-            & (df[unaffected_partner] == "BB")
-            & (df[affected_partner] == "AB"),
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "AA"),
             # Criteria 2
             (df[reference] == "BB")
-            & (df[unaffected_partner] == "AA")
-            & (df[affected_partner] == "AB"),
-            # Criteria to label low Risk SNPs if reference affected, or high risk SNPs if reference unaffected
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "BB"),
             # Criteria 3
             (df[reference] == "AA")
-            & (df[unaffected_partner] == "AA")
-            & (df[affected_partner] == "AB"),
+            & (df[male_partner] == "AA")
+            & (df[female_partner] == "AB"),
             # Criteria 4
             (df[reference] == "BB")
-            & (df[unaffected_partner] == "BB")
-            & (df[affected_partner] == "AB"),
+            & (df[male_partner] == "BB")
+            & (df[female_partner] == "AB"),
+            # Criteria 5
+            (df[reference] == "AB")
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "AA"),
+            # Criteria 6
+            (df[reference] == "AB")
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "BB"),
+            # Criteria 7
+            (df[reference] == "AB")
+            & (df[male_partner] == "AA")
+            & (df[female_partner] == "AB"),
+            # Criteria 8
+            (df[reference] == "AB")
+            & (df[male_partner] == "BB")
+            & (df[female_partner] == "AB"),
+            # Criteria 9
+            (df[reference] == "AA" | df[reference] == "BB")
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "AB"),
         ]
         # Assign the correct labels depending upon reference status
         if reference_status == "affected":
             values = [
-                "high_risk",  # Criteria 1
-                "high_risk",  # Criteria 2
-                "low_risk",  # Criteria 3
-                "low_risk",  # Criteria 4
+                "male_partner_low_risk",  # Criteria 1
+                "male_partner_low_risk",  # Criteria 2
+                "female_partner_low_risk",  # Criteria 3
+                "female_partner_low_risk",  # Criteria 4
+                "male_partner_high_risk",  # Criteria 5
+                "male_partner_high_risk",  # Criteria 6
+                "female_partner_high_risk",  # Criteria 7
+                "female_partner_high_risk",  # Criteria 8
+                "low_&_high_risk",  # Criteria 9
             ]
-        elif reference_status == "unaffected":
-            values = [
-                "low_risk",  # Criteria 1
-                "low_risk",  # Criteria 2
-                "high_risk",  # Criteria 3
-                "high_risk",  # Criteria 4
-            ]
-        df["snp_risk_category"] = np.select(
-            conditions, values, default="uninformative"
-        ).copy()
-    elif reference_relationship in [
-        "child",  # TODO populate with all appropriate relationships
-    ]:
+
+        df["snp_risk_category"] = np.select(conditions, values, default="uninformative")
+
+    elif reference_status == "unaffected":
         # Label alleles as high or low risk
         conditions = [
-            # Criteria to label high Risk SNPs if reference affected, or low risk SNPs if reference unaffected
+            # Criteria to label low Risk SNPs if reference affected, or low risk SNPs if reference unaffected
             # Criteria 1
-            (df[reference] == "AB")
-            & (df[unaffected_partner] == "AA")
-            & (df[affected_partner] == "AB"),
+            (df[reference] == "AA" | df[reference] == "BB")
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "AA" | df[female_partner] == "BB"),
             # Criteria 2
             (df[reference] == "AB")
-            & (df[unaffected_partner] == "BB")
-            & (df[affected_partner] == "AB"),
-            # Criteria to label low Risk SNPs if reference affected, or high risk SNPs if reference unaffected
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "AA" | df[female_partner] == "BB"),
             # Criteria 3
             (df[reference] == "AA")
-            & (df[unaffected_partner] == "AA")
-            & (df[affected_partner] == "AB"),
+            & (df[male_partner] == "AA")
+            & (df[female_partner] == "AB"),
             # Criteria 4
             (df[reference] == "BB")
-            & (df[unaffected_partner] == "BB")
-            & (df[affected_partner] == "AB"),
+            & (df[male_partner] == "BB")
+            & (df[female_partner] == "AB"),
+            # Criteria 5
+            (df[reference] == "AB" | df[reference] == "AA" | df[reference] == "BB")
+            & (df[male_partner] == "AB")
+            & (df[female_partner] == "AA" | df[female_partner] == "AA"),
+            # Criteria 6
+            (df[reference] == "AB" | df[reference] == "AA" | df[reference] == "BB")
+            & (df[male_partner] == "AA" | df[reference] == "BB")
+            & (df[female_partner] == "AB"),
         ]
         # Assign the correct labels depending upon reference status
-        if reference_status == "affected":
+        if reference_status == "unaffected":
             values = [
-                "high_risk",  # Criteria 1
-                "high_risk",  # Criteria 2
-                "low_risk",  # Criteria 3
-                "low_risk",  # Criteria 4
+                "male_parther_high_risk",  # Criteria 1
+                "male_parther_low_risk",  # Criteria 2
+                "female_parther_high_risk",  # Criteria 3
+                "female_parther_low_risk",  # Criteria 4
+                "high_&_low_risk",  # Criteria 5
+                "high_&_low_risk",  # Criteria 6
             ]
-        elif reference_status == "unaffected":
-            values = [
-                "low_risk",  # Criteria 1
-                "low_risk",  # Criteria 2
-                "high_risk",  # Criteria 3
-                "high_risk",  # Criteria 4
-            ]
-        df["snp_risk_category"] = np.select(
-            conditions, values, default="uninformative"
-        ).copy()
+
+        df["snp_risk_category"] = np.select(conditions, values, default="uninformative")
+
     return df
