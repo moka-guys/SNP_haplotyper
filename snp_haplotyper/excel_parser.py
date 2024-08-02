5a7ba6ac7ac3ba8bddbe43b5e4d41d9d05e5a026
graeme-smith <graeme_c_smith@hotmail.com>
2024-08-02 20:29:01 +0100

    Deleted commented out code

--- snp_haplotyper/excel_parser.py
+++ snp_haplotyper/excel_parser.py
@@ -213,7 +213,6 @@ def parse_excel_input(input_spreadsheet, snp_array_file=None):
             # Process cell locations in the format data_entry!$B$31 or data_entry!$F$22:$L$22 (merged cells)
             cell_location = dn.attr_text.split(":")[0].split("!")[1].replace("$", "")
             cell_value = str(data_entry_sheet[cell_location].value).strip()
-            # print(cell_location, cell_value)
             argument_dict[input_name] = cell_value
 
     biopsy_number = argument_dict["biopsy_number"]
